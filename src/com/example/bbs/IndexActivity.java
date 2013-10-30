223c063f4565f66cfcb4a873783559b2e9d5ffae
wang153723482 <wang153723482@gmail.com>
2013-10-30 08:41:16 +0000

    增加第二个activity，完成从第一个activity到第二个的跳转

--- src/com/example/bbs/IndexActivity.java
+++ src/com/example/bbs/IndexActivity.java
@@ -1,10 +1,11 @@
 package com.example.bbs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import com.example.bbs.zol.IndexZolActivity;
 
 public class IndexActivity extends Activity {
     /** Called when the activity is first created. */
@@ -16,27 +17,45 @@ public class IndexActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        editText = (EditText) findViewById(R.layout.main);
+        editText = (EditText) findViewById(R.id.editText);
 
-        //第一种方式的监听
+        /*//第一种方式的监听
         Button btType = (Button) findViewById(R.id.btZol);
-        btType.setOnClickListener(listener);
+        btType.setOnClickListener(listener);*/
     }
 
-    //第一种方式监听
+    /*//第一种方式监听
     Button.OnClickListener listener = new Button.OnClickListener(){//创建监听对象
         public void onClick(View v){
             i++;
             String strTmp = "第"+i+"次点击了Button";
             editText.setText(strTmp);
         }
-    };
+    };*/
 
+
+    //第二种方式的监听 在mail.xml注册监听事件
+    public void btZolOnClick(View v){
+        i++;
+        String strTmp = "第"+i+"次点击了Button(zol)";
+        editText.setText(strTmp);
+
+        Intent intent = new Intent();
+        intent.putExtra("键值对名","要传的值");//向目的Activity传值，不传值时不用写
+        intent.setClass(IndexActivity.this, IndexZolActivity.class);
+        IndexActivity.this.startActivity(intent);
+//        this.finish();//注销第一页，可以不用
+
+    }
+
+    //在twoActivity中取值：
+    //Intent intent = getIntent();
+    //String value = intent.getStringExtra("键值对名");
 
     //第二种方式的监听 在mail.xml注册监听事件
     public void btFengNiaoOnClick(View v){
         i++;
-        String strTmp = "第"+i+"次点击了Button";
+        String strTmp = "第"+i+"次点击了Button(fengniao)";
         editText.setText(strTmp);
     }
 }
---
+++ src/com/example/bbs/zol/IndexZolActivity.java
@@ -0,0 +1,20 @@
+package com.example.bbs.zol;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.bbs.R;
+
+/**
+ * @author: wangchao
+ * Date: 13-10-30 обнГ4:20
+ * Description: 
+ */
+public class IndexZolActivity  extends Activity {
+    /** Called when the activity is first created. */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index_zol);
+    }
+}
